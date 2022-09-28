395f00ed761fa0306d67dad50217925fed35a199
Luis Felipe <luisfelipearaujopimenta@gmail.com>
2022-09-28 20:55:16 -0300

    first commit

---
+++ gerar_pdf.php
@@ -0,0 +1,28 @@
+<?php
+
+// Carregar o Composer
+require './vendor/autoload.php';
+
+// Referenciar o namespace Dompdf
+use Dompdf\Dompdf;
+
+// Instanciar e usar a classe dompdf
+$dompdf = new Dompdf(['enable_remote' => true]);
+
+
+$dados = file_get_contents(__DIR__ . '/teste.php');
+
+// Instanciar o metodo loadHtml e enviar o conteudo do PDF
+$dompdf->loadHtml($dados);
+
+// Configurar o tamanho e a orientacao do papel
+// landscape - Imprimir no formato paisagem
+//$dompdf->setPaper('A4', 'landscape');
+// portrait - Imprimir no formato retrato
+$dompdf->setPaper('A4', 'portrait');
+
+// Renderizar o HTML como PDF
+$dompdf->render();
+
+// Gerar o PDF
+$dompdf->stream();
